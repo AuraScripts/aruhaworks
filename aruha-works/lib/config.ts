8b7cf7d9553776add73e80d6ad08fd4720044003
Aura Scripts <zakrochannel15@gmail.com>
2026-08-19 00:37:17 +0300

    Delete aruha-works directory

--- aruha-works/lib/config.ts
+++
@@ -1,4 +0,0 @@
-// Update this once your GitBook space is ready. Leave the placeholder and
-// the Documentation link will still work, it'll just point nowhere useful
-// until you swap it in.
-export const GITBOOK_URL = "https://aruha-works.gitbook.io/aruha-works-docs";
